abce93fdf1e4b8492461b96d25a4137b017340df
Nicolas Rios <nicolas.rios@icareweb.com>
2022-08-18 17:33:22 +0200

    Clean and improve comments

--- src/kyu5/pigLatin/index.ts
+++ src/kyu5/pigLatin/index.ts
@@ -1,3 +1,11 @@
+/**
+ Simple Pig Latin
+ * Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+ * Examples
+ * pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+ * pigIt('Hello world !');     // elloHay orldway !
+ */
+
 const pigIt = (a : string) => a.replace(/(\w)(\w+)*/g, "$2$1ay")
 
 console.log(pigIt('Hello world!'))
